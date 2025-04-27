1349e0a7ae520817b3689d8aee0d21d9652e3ec5
aliazeemh <ali.azeem@ideas.com.pk>
2025-04-27 12:59:28 +0500

    Add PWA to app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules: [
+    '@nuxtjs/pwa',
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
@@ -11,5 +12,43 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/ui'
-  ]
+  ],
+  css: [
+    '@ionic/core/css/ionic.bundle.css',
+  ],
+  build: {
+    transpile: ['@ionic/vue', '@ionic/vue-router'],
+  },
+  runtimeConfig: {
+    pwa: {
+      manifest: {
+        name: 'Elryan',
+        short_name: 'elryan',
+        lang: 'en',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/manifest/ElRyan.ico',
+            sizes: '128x128',
+            type: 'image/icon'
+          }
+        ]
+      },
+      meta: {
+        theme_color: '#ffffff',
+      },
+      workbox: {
+        offlineAnalytics: true,
+        runtimeCaching: [
+          {
+            urlPattern: 'http://localhost:3001/*',
+            handler: 'NetworkFirst',
+            method: 'GET',
+          }
+        ]
+      }  
+    }
+  }
 })
